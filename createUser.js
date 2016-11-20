7e7c418913bac9758503e32e7c3c18d079c7741d
ibanez195 <ibanez195@gmail.com>
2016-11-20 09:53:45 -0500

    fix createUser formatting

--- createUser.js
+++ createUser.js
@@ -20,40 +20,38 @@ function createUser(firstName, lastName, acctName, accountJson) {
                     customer = person;
 
 
-            // use found customer id to get account list url
-            var account_url = "http://api.reimaginebanking.com/customers/" + customer["_id"]
-                + "/accounts?key=355f3102e837501d7620a220cd1ebd37";
+                    // use found customer id to get account list url
+                    var account_url = "http://api.reimaginebanking.com/customers/" + customer["_id"]
+                        + "/accounts?key=355f3102e837501d7620a220cd1ebd37";
 
-            // begin GET request for account list
-            $.ajax({
-                url: account_url,
-                type: "get",
-                headers: {
-                    "Accept": "applications/json"
-                },
-                success: function(response){
-                    // loop through response and find account with matching nickname
-                    response.forEach(function(account){
-                        // once found set account json object to correct
-                        if(account["nickname"] === acctName){
+                    // begin GET request for account list
+                    $.ajax({
+                        url: account_url,
+                        type: "get",
+                        headers: {
+                            "Accept": "applications/json"
+                        },
+                        success: function(response){
+                            // loop through response and find account with matching nickname
+                            response.forEach(function(account){
+                                // once found set account json object to correct
+                                if(account["nickname"] === acctName){
 
-                            accountJson = {
-                                "first_name": customer["first_name"],
-                                "last_name": customer["last_name"],
-                                "id": customer["_id"],
-                                "account_id": account["_id"]
-                            }
-                            chrome.storage.sync.set({
-                              credentials: accountJson
-                            });
-                        }
-                    })
-                },
-
-            });
-
-          }
-      })
+                                    accountJson = {
+                                        "first_name": customer["first_name"],
+                                        "last_name": customer["last_name"],
+                                        "id": customer["_id"],
+                                        "account_id": account["_id"]
+                                    }
+                                    chrome.storage.sync.set({
+                                      credentials: accountJson
+                                    });
+                                }
+                            })
+                        },
+                    });
+                }
+            })
         }
     });
 }
